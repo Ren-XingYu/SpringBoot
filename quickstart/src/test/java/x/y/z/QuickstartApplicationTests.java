eae16a8ee6400ee15c0433d205cd0de6acce2cfe
Ren-XingYu <379271608@qq.com>
2023-08-29 07:02:27 +0800

    quickstart

---
+++ quickstart/src/test/java/x/y/z/QuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package x.y.z;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
